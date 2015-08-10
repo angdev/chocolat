94accdea4d3504d4eb2282264f9d6aeb372ecb3a
Hong ChulJu <ang0123dev@gmail.com>
2015-08-11 01:42:19 +0900

    Cmd: Project creation

--- main.go
+++ main.go
@@ -1,24 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/angdev/chocolat/api"
 	"github.com/angdev/chocolat/model"
 	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/codegangsta/cli"
 	"github.com/joho/godotenv"
 	"github.com/k0kubun/pp"
+	"github.com/satori/go.uuid"
 	"net/http"
+	"os"
 )
 
-func main() {
+func initialize() {
 	err := godotenv.Load()
 	if err != nil {
 		pp.Print(err.Error())
 	}
-
 	model.InitDB()
+}
 
-	db := model.DB()
-	db.LogMode(true)
+func run() {
+	initialize()
 
 	apiServer := rest.NewApi()
 	apiServer.Use(rest.DefaultDevStack...)
@@ -59,4 +63,44 @@ func mergeRouteSet(routeSets ...[]*rest.Route) []*rest.Route {
 		routes = append(routes, routeSet...)
 	}
 	return routes
+}
+
+func createProject() {
+	initialize()
+
+	db := model.DB()
+	project := model.Project{
+		UUID: uuid.NewV4().String(),
+	}
+	db.Create(&project)
+
+	fmt.Printf("Created a new project.\n")
+	fmt.Printf("Project UUID - %s\n", project.UUID)
+	fmt.Printf("Project Master Key - %s\n", project.MasterKey().Value)
+	fmt.Printf("Project Read Key - %s\n", project.ReadKey().Value)
+	fmt.Printf("Project Write Key - %s\n", project.WriteKey().Value)
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "chocolat"
+	app.Usage = "Yet Another Data Aggregation Server"
+	app.Version = "0.0.1"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "create, c",
+			Value: "project",
+			Usage: "Create a new project",
+		},
+	}
+
+	app.Action = func(c *cli.Context) {
+		if c.String("create") == "project" {
+			createProject()
+		} else {
+			run()
+		}
+	}
+
+	app.Run(os.Args)
 }
