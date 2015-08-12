d7617daeb80d42fca20b889cee384d5bc5438a8b
Hong ChulJu <ang0123dev@gmail.com>
2015-08-12 17:25:49 +0900

    Add cli commands related with project

--- main.go
+++ main.go
@@ -81,24 +81,73 @@ func createProject() {
 	fmt.Printf("Project Write Key - %s\n", project.WriteKey().Value)
 }
 
+func listProject() {
+	initialize()
+
+	db := model.DB()
+	var projects []model.Project
+
+	if db.Find(&projects).RecordNotFound() {
+		fmt.Println("No project found")
+		return
+	}
+
+	for _, p := range projects {
+		fmt.Println(p.UUID)
+	}
+}
+
+func inspectProject(uuid string) {
+	initialize()
+
+	project := model.ProjectByUUID(uuid)
+
+	if project == nil {
+		fmt.Println("No project found")
+		return
+	}
+
+	fmt.Printf("Project UUID - %s\n", project.UUID)
+	fmt.Printf("Project Master Key - %s\n", project.MasterKey().Value)
+	fmt.Printf("Project Read Key - %s\n", project.ReadKey().Value)
+	fmt.Printf("Project Write Key - %s\n", project.WriteKey().Value)
+}
+
 func main() {
 	app := cli.NewApp()
 	app.Name = "chocolat"
 	app.Usage = "Yet Another Data Aggregation Server"
 	app.Version = "0.0.1"
 	app.Flags = []cli.Flag{
-		cli.StringFlag{
+		cli.BoolFlag{
 			Name:  "create, c",
-			Value: "project",
 			Usage: "Create a new project",
+		},
+		cli.BoolFlag{
+			Name:  "run, r",
+			Usage: "Run the server",
+		},
+		cli.BoolFlag{
+			Name:  "list, l",
+			Usage: "Listing projects",
+		},
+		cli.StringFlag{
+			Name:  "project, p",
+			Usage: "Inspect a project",
 		},
 	}
 
 	app.Action = func(c *cli.Context) {
-		if c.String("create") == "project" {
+		if c.Bool("create") {
 			createProject()
-		} else {
+		} else if c.Bool("list") {
+			listProject()
+		} else if c.String("project") != "" {
+			inspectProject(c.String("project"))
+		} else if c.Bool("run") {
 			run()
+		} else {
+			cli.ShowAppHelp(c)
 		}
 	}
 
