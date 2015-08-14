fb3459df605e5da5bf46f16fb887a31140b4fb66
Hong ChulJu <ang0123dev@gmail.com>
2015-08-15 03:19:58 +0900

    QueryParams: Timeframe full implementation

--- api/query_params.go
+++ api/query_params.go
@@ -61,7 +61,7 @@ func (t *TimeFrame) UnmarshalJSON(data []byte) error {
 		t.absolute = true
 		return nil
 	} else if err = json.Unmarshal(data, &rel); err == nil {
-		return errors.New("Relative timeframe is not implemented")
+		return t.parse(rel)
 	} else {
 		return InvalidTimeFrameError
 	}
@@ -69,6 +69,142 @@ func (t *TimeFrame) UnmarshalJSON(data []byte) error {
 
 func (this *TimeFrame) IsGiven() bool {
 	return !(this.Start.IsZero() && this.End.IsZero())
+}
+
+func (this *TimeFrame) parse(relative string) error {
+	ok := false
+
+	if ok = this.parseShortHand(relative); ok {
+		return nil
+	}
+
+	params := strings.Split(relative, "_")
+	switch len(params) {
+	case 2:
+		ok = this.parseRelUnits(params[0], params[1])
+	case 3:
+		if n, err := strconv.Atoi(params[1]); err == nil {
+			ok = this.parseRelNUnits(params[0], n, params[2])
+		}
+	}
+
+	if ok {
+		return nil
+	} else {
+		return InvalidTimeFrameError
+	}
+}
+
+func (this *TimeFrame) parseShortHand(s string) bool {
+	switch s {
+	case "today":
+		return this.parseRelUnits("this", "day")
+	case "previous_minute":
+		return this.parseRelNUnits("previous", 1, "minutes")
+	case "previous_hour":
+		return this.parseRelNUnits("previous", 1, "hours")
+	case "yesterday":
+		return this.parseRelNUnits("previous", 1, "days")
+	case "previous_day":
+		return this.parseRelNUnits("previous", 1, "days")
+	case "previous_week":
+		return this.parseRelNUnits("previous", 1, "weeks")
+	case "previous_month":
+		return this.parseRelNUnits("previous", 1, "months")
+	case "previous_year":
+		return this.parseRelNUnits("previous", 1, "years")
+	}
+
+	return false
+}
+
+func (this *TimeFrame) parseRelUnits(rel string, units string) bool {
+	if rel != "this" {
+		return false
+	}
+
+	switch units {
+	case "minute":
+		this.Start = now.BeginningOfMinute()
+	case "hour":
+		this.Start = now.BeginningOfHour()
+	case "day":
+		this.Start = now.BeginningOfDay()
+	case "week":
+		this.Start = now.BeginningOfWeek()
+	case "month":
+		this.Start = now.BeginningOfMonth()
+	case "year":
+		this.Start = now.BeginningOfYear()
+	default:
+		return false
+	}
+
+	this.End = time.Now()
+	return true
+}
+
+func (this *TimeFrame) parseRelNUnits(rel string, n int, units string) bool {
+	if rel == "this" {
+		return this.parseThisNUnits(n, units)
+	} else if rel == "previous" {
+		return this.parsePreviousNUnits(n, units)
+	} else {
+		return false
+	}
+}
+
+func (this *TimeFrame) parseThisNUnits(n int, units string) bool {
+	n = n - 1
+	switch units {
+	case "minutes":
+		this.Start = now.BeginningOfMinute().Add(time.Duration(-n) * time.Minute)
+		this.End = now.EndOfMinute()
+	case "hours":
+		this.Start = now.BeginningOfHour().Add(time.Duration(-n) * time.Hour)
+		this.End = now.EndOfHour()
+	case "days":
+		this.Start = now.BeginningOfDay().AddDate(0, 0, -n)
+		this.End = now.EndOfDay()
+	case "weeks":
+		this.Start = now.BeginningOfWeek().AddDate(0, 0, -n*7)
+		this.End = now.EndOfWeek()
+	case "months":
+		this.Start = now.BeginningOfMonth().AddDate(0, -n, 0)
+		this.End = now.EndOfMonth()
+	case "years":
+		this.Start = now.BeginningOfYear().AddDate(-n, 0, 0)
+		this.End = now.EndOfYear()
+	default:
+		return false
+	}
+	return true
+}
+
+func (this *TimeFrame) parsePreviousNUnits(n int, units string) bool {
+	switch units {
+	case "minutes":
+		this.Start = now.BeginningOfMinute().Add(time.Duration(-n) * time.Minute)
+		this.End = now.BeginningOfMinute()
+	case "hours":
+		this.Start = now.BeginningOfHour().Add(time.Duration(-n) * time.Hour)
+		this.End = now.BeginningOfHour()
+	case "days":
+		this.Start = now.BeginningOfDay().AddDate(0, 0, -n)
+		this.End = now.BeginningOfDay()
+	case "weeks":
+		this.Start = now.BeginningOfWeek().AddDate(0, 0, -n*7)
+		this.End = now.BeginningOfWeek()
+	case "months":
+		this.Start = now.BeginningOfMonth().AddDate(0, -n, 0)
+		this.End = now.BeginningOfMonth()
+	case "years":
+		this.Start = now.BeginningOfYear().AddDate(-n, 0, 0)
+		this.End = now.BeginningOfYear()
+	default:
+		return false
+	}
+	return true
 }
 
 type GroupBy []string
